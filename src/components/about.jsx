828c21c09e6c2d1ee5b12f24db7cfb3ecee8d0fe
Lasha Kvantaliani <lasharela@gmail.com>
2022-12-16 05:34:13 +0400

    text update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,55 +11,17 @@ export const About = () => {
             <h2 className="text-4xl font-medium sm:text-5xl">About app</h2>
             <div className="space-y-4 text-lg text-gray-500">
               <p>
-                The New York Times bestseller book{" "}
-                <a
-                  target={"_blank"}
-                  href="https://jamesclear.com/atomic-habits"
-                >
-                  Atomic Habits
-                </a>
-                , which has sold more than 9 million copies worldwide, described
-                a very simple task management methodology invented by{" "}
-                <span className="font-semibold">Ivy Lee</span>.
-              </p>
-
-              <p>
-                The method of prioritizing your to-do list seems stupidly
-                simple. Here's what makes it so effective:
-              </p>
-
-              <ul className="list-disc pl-4">
-                <li>
-                  <span className="font-bold">
-                    It's simple enough to actually work
-                  </span>{" "}
-                  - Use simple rules to guide complex behavior.
-                </li>
-                <li>
-                  <span className="font-bold">
-                    It forces you to make tough decisions
-                  </span>{" "}
-                  - Trim away everything that isn't absolutely necessary
-                </li>
-                <li>
-                  <span className="font-bold">
-                    It removes the friction of starting
-                  </span>{" "}
-                  - Forces you to decide on your first task the night before you
-                  go to work
-                </li>
-                <li>
-                  <span className="font-bold">
-                    It requires you to single-task
-                  </span>{" "}
-                  - The method will force you to focus on one task at a time
-                </li>
-              </ul>
-              <p>
-                Read more about the method and the inspiring{" "}
+                <i>Atomic Habits</i> is a best-selling book that has sold over 9
+                million copies worldwide. It introduces a task management method
+                created by <i>Ivy Lee</i> that is simple but effective. The
+                method involves prioritizing your to-do list and following
+                simple rules to guide your behavior, making decisions which 6
+                tasks to do the night before, and focusing on one task at a
+                time.{" "}
                 <a target={"_blank"} href="https://jamesclear.com/ivy-lee">
-                  story of Ivy Lee{" "}
-                </a>
+                  Learn more
+                </a>{" "}
+                about the method and Ivy Lee's story.
               </p>
             </div>
             <ActionButtons />
